271bfdf6406f8e1296521952810d417f760ec7ea
Rohan Fernandes <rohanf@rediffmail.com>
2013-03-16 19:20:34 -0700

    Updated copyright message

--- common/bottom/footer.php
+++ common/bottom/footer.php
@@ -1,7 +1,7 @@
       <table border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse" bordercolor="#111111" width="779" id="AutoNumber20" height="26" background="/images/bg-bottom.gif">
         <tr>
           <td>
-          <p style="margin-left: 20"><font color="#FFFFFF" size="1">&copy;Copyright. Animal Angels Foundation. All rights reserved. Photography: Sameer Mangtani</font></td>
+          <p style="margin-left: 20"><font color="#FFFFFF" size="1">&copy; <?php echo date("Y"); ?> Animal Angels Foundation. All rights reserved. Photography: Sameer Mangtani</font></td>
           <td>
             <p align="right" style="margin-right: 20">
             <font color="#FFFFFF" size="1">Site created by:
