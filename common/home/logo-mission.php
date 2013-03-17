a3f37a10b471c1c455e8a596b1ca6031b4e70958
Rohan Fernandes <rohanf@rediffmail.com>
2013-03-16 22:58:06 -0700

    Updated social sharing bar

--- common/home/logo-mission.php
+++ common/home/logo-mission.php
@@ -9,8 +9,8 @@
 
       <div class='divOuter'>
 
-      <!-- BEGIN GOOGLE TRANSLATE -->
-                <div class='divInner1' style="border-top: 4px solid transparent;" id="google_translate_element"></div>
+        <!-- BEGIN GOOGLE TRANSLATE -->
+        <div class='divInner1' style="border-top: 4px solid transparent; width: 158px;" id="google_translate_element"></div>
 		<script>
                 function googleTranslateElementInit() {
                   new google.translate.TranslateElement({
@@ -20,30 +20,59 @@
                     layout: google.translate.TranslateElement.InlineLayout.SIMPLE
                   }, 'google_translate_element');
                 }
-                </script>
-                <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-      <!-- END GOOGLE TRANSLATE -->
+        </script>
+        <script src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+        <!-- END GOOGLE TRANSLATE -->
 
-        <div class='divInner2' style="border: 6px solid transparent;"><a href="http://twitter.com/share" class="twitter-share-button" data-count="horizontal">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script></div>
-
-        <div class='divInner3' style="border: 6px solid transparent;"><div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:like href="" send="false" layout="button_count" width="5" show_faces="true" action="like" font="arial"></fb:like></div>
-
-        <div class='divInner4' style="border: 7px solid transparent;"><g:plusone size="medium"></g:plusone></div>
-		<!--  Place this tag after the last plusone tag -->
-		<script type="text/javascript">
-		  (function() {
-		    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-		    po.src = 'https://apis.google.com/js/plusone.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-		  })();
-		</script>
-
+        <!-- BEGIN TWITTER -->
+        <div class='divInner2' style="border: 6px solid transparent; width: 80px;">
+			<a href="https://twitter.com/share" class="twitter-share-button" data-via="AngelsWoof" data-count="none">Tweet</a>
+			<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+        </div>
+        <!-- END TWITTER -->
+        
+        <!-- BEGIN FACEBOOK -->
+        <div class='divInner3' style="border: 6px solid transparent; width: 130px;">
+        	<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+				var js, fjs = d.getElementsByTagName(s)[0];
+				if (d.getElementById(id)) return;
+				js = d.createElement(s); js.id = id;
+				js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+				fjs.parentNode.insertBefore(js, fjs);
+			}(document, 'script', 'facebook-jssdk'));
+    	    </script>
+        	<fb:like send="true" layout="button_count" width="5" show_faces="true" font="arial"></fb:like>
+        </div>
+        <!-- END FACEBOOK -->
+        
+        <!-- BEGIN GOOGLE PLUS -->
+        <div class='divInner4' style="border: 7px solid transparent; width: 60px;">
+			<div class="g-plusone" data-size="medium" data-annotation="none"></div>
+			<!-- Place this tag after the last +1 button tag. -->
+			<script type="text/javascript">
+			  (function() {
+			    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+			    po.src = 'https://apis.google.com/js/plusone.js';
+			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+			  })();
+			</script>
+	        </div>  
+        <!-- END GOOGLE PLUS -->
+        
+        <!-- BEGIN LINKEDIN -->
+        <div class='divInner5' style="border: 6px solid transparent; width: 70px;">
+			<script src="//platform.linkedin.com/in.js" type="text/javascript"></script>
+			<script type="IN/Share"></script>
+        </div>
+        <!-- END LINKEDIN -->
 
       </div>
 
 <?
     }
 ?>
+
               </td>
             </tr>
             <tr>
